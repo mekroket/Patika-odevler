23df3da1d7c454b52bf6bb50d566b69fa8f52087
mekroket <67954467+mekroket@users.noreply.github.com>
2021-06-29 13:34:49 +0300

    homework push

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-"# Patika--devler" 
+# Patika NodeJs Backend Patikasi Ödevleri
+
+## Proje Amacı 
+Yazacağınız Javascript dosyasında Date methodunu kullanacaksınız. Detaylı bilgi için tıklayınız.
+
+Gün olarak aldığınız değeri string olarak göstermeniz gerekmektedir. (İpucu: 1 = Pazartesi)
+
+Eğer saat, dakika ya da saniye 10'dan küçükse başına 0'ı koymalısınız.
+
+Her 1 saniyede bir yenilenmelidir.
+
+Kullanıcıdan alınan ad direkt olarak ekrana yazılmalıdır.
+</br>
+
+
+# Olması Gereken ;
+```
+http://cengizcmataraci.me/javascriptclock/
+```
---
+++ Ödev-1/main.js
@@ -0,0 +1,49 @@
+//!İlk öncelikle kullanıcı girdisini istedik
+//!daha sonra saat için bir fonksiyon yazdık saat fonskiyonunda öncelikle date ile zaman değerini oluşturuyoruz
+//* kullanıcı girdisini fonskiyon olarak yazarsak daha uygun oluyor
+
+function isimGöster() {
+    let name = prompt("Adınızı Giriniz")
+    let namewrite = document.querySelector("#isim")
+    namewrite.innerHTML = `${name}`
+}
+
+function Saat() {
+    //zamand değerlerini oluşturuyoruz
+    let date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    var session = date.getDay();
+
+    //günleri yazdırmak için day değerini ele alıp index değerine göre günleri yazdırdık
+    if (session == 1) {
+        session = "Pazartesi";
+    } else if (session == 2) {
+        session = "Salı";
+    } else if (session == 3) {
+        session = "Çarşamba";
+    } else if (session == 4) {
+        session = "Perşembe";
+    } else if (session == 5) {
+        session = "Cuma";
+    } else if (session == 6) {
+        session = "Cumartesi";
+    } else if (session == 7) {
+        session = "Pazar";
+    }
+
+    //daha sonra saat dakika ve saniye için  koşulları yazdık
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+
+    var time = h + ":" + m + ":" + s + " " + session; //burda ise zaman değerlerinin ekrana yazım şeklini yazdırdık
+    document.getElementById("tarih").innerText = time;
+    document.getElementById("tarih").textContent = time;
+
+    setTimeout(Saat, 1000); //zaman ayarlı işlemi 1000 ile başlatıyoruz
+}
+
+isimGöster();
+Saat();
